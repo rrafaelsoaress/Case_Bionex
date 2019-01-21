424da7db86a4b3da0c9190f616867d27e83124a1
Rafael <rafael-.s@hotmail.com>
2019-01-21 10:51:43 -0200

    ajuste no login

--- features/specs/login.feature
+++ features/specs/login.feature
@@ -1,9 +1,9 @@
 #language: pt
 #utf-8
-
+@login
 Funcionalidade: Efetuar login
 
     Cenario: Efetuar login corretamente
         Dado que eu acesse o sistema
         Quando faço login
-        Então sou direcionado para a home do site
+        Então sou direcionado para a home do site com a mensagem "Autenticado com sucesso."
--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -7,7 +7,11 @@ Quando("faço login") do
     @login_page.logar
 end
   
-Então("sou direcionado para a home do site") do
-    sucesso = assert_text("Autenticado com sucesso.")
-    expect(sucesso).to eql sucesso    
+# Então("sou direcionado para a home do site") do
+#     # sucesso = assert_text("Autenticado com sucesso.")
+#     # expect(sucesso).to eql sucesso    
+# end
+
+Então("sou direcionado para a home do site com a mensagem {string}") do |sucesso|
+    expect(sucesso).to have_content sucesso
 end
